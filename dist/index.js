5f9b2507c7b46ee9b9fedc54b9c58c648e7d7710
Namdev Kadam <namdevkadam6266@gmail.com>
2026-08-12 10:51:49 +0530

    first commit

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function great(person) {
+    return `helo ${person}, wellcome to me deshbord`;
+}
+const username = "Namdev";
+console.log(great(username));
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+function great(person:string):string{
+    return `helo ${person}, wellcome to me deshbord`
+}
+const username:string ="Namdev"
+console.log(great(username));
