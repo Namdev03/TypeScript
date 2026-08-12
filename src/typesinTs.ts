da1df56df4a699cf357d094bab858ceeb437c77f
Namdev <namdevkadem6266@gmail.com>
2026-08-12 11:27:41 +0530

    Add initial TypeScript files with variable declarations and basic structure

---
+++ src/typesinTs.ts
@@ -0,0 +1,4 @@
+let drink = "chai";
+let channelName=""
+let chaiFlavour:string ="masala chai";
+chaiFlavour = "ginger tea";
---
+++ src/unianAndany.ts
@@ -0,0 +1,10 @@
+let subs:number | string = "1M"
+let apiRequest:"pending"|'success'|'error'= 'pending'
+apiRequest ="pending"
+const orders=[12,'23',44]
+for(let oder of orders){
+    
+}
+
+let name:string;
+name ="Namdev"
