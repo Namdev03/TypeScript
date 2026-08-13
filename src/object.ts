54463410582148d1c7d0496e60ae5e06a5f8db2c
Namdev <namdevkadem6266@gmail.com>
2026-08-13 09:57:31 +0530

    Add tea interface and initial chai objects with properties

---
+++ src/object.ts
@@ -0,0 +1,20 @@
+interface tea{
+    name:string,
+    price:number,
+    isHot:boolean
+}
+const chai:tea = {
+    name:"masala chai",
+    price:20,
+    isHot:true
+}
+type Tea ={
+     name:string,
+    price:number,
+    ingredient:string[]
+}
+const adrakChai :Tea ={
+    name:"adrak",
+    price:40,
+    ingredient:["namde","kadam"]
+}
