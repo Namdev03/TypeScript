9ef494a05db1004f8b4b3b5d6b32ea2055cdcf05
Namdev <namdevkadem6266@gmail.com>
2026-08-15 06:20:00 +0530

    chai ban gya hai

--- dist/interfaceandGen.d.ts
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=interfaceandGen.d.ts.map
--- dist/interfaceandGen.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function pair(a, b) {
-    return [a, b];
-}
-pair("masala", "test");
-//# sourceMappingURL=interfaceandGen.js.map
